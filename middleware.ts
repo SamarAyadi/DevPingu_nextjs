257d4869b82663e28909af6332f2d4329e2ece06
Samar Ayadi <ayadisamardev@gmail.com>
2024-04-27 11:16:20 +0100

    few change The clerk page is not accessible in Tunisia

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
 
-export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+
+const isProtectedRoute = createRouteMatcher(["/ask-question"]);
+export default clerkMiddleware((auth, req) => {  if (isProtectedRoute(req)) auth().protect();});
+export const config = {  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],};
