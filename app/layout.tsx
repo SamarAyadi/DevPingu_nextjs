f88c54c893f017b07c031df715dcc01cf787b332
Samar Ayadi <ayadisamardev@gmail.com>
2024-05-01 21:15:39 +0100

    Theme Switcher and Mobile Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import type { Metadata } from "next";
@@ -23,7 +24,7 @@ const spaceGrotesk = Space_Grotesk({
 export const metadata: Metadata = {
   title: "DevPingu",
   description:
-    "A community-driven platform for asking and answering programming questions Get help, share knowledge , and collaborate with developers from around the world",
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
   icons: {
     icon: "/assets/images/site-logo.svg",
   },
